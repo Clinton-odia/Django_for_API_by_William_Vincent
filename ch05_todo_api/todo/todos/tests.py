74d4f27219a3a55fc4aee7115065f38a7f7065bf
Clinton Odia <odiamehiclinton@gmail.com>
2025-08-25 15:32:44 +0100

    Test: todo model

--- ch05_todo_api/todo/todos/tests.py
+++ ch05_todo_api/todo/todos/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
-
+from .models import Todo
 # Create your tests here.
+
+
+class TodoModelTest(TestCase):
+    def setUp(self):
+        self.todo = Todo.objects.create(
+            title="learn django",
+            body="important",
+        )
+
+    def test_todo_content(self):
+        self.assertEqual(self.todo.title, "learn django")
+        self.assertEqual(self.todo.body, "important")
+        self.assertEqual(str(self.todo), "learn django")
